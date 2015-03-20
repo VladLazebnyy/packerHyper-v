ebe4d4bcf925ef92696b3f0a0cad0730c6ecfdde
VladLazebnyy <vlad.lazebnyy@gmail.com>
2015-03-20 15:18:52 +0200

    first iteration of debug - rewrite params that put in function

--- packer/builder/hyperv/iso/builder.go
+++ packer/builder/hyperv/iso/builder.go
@@ -305,8 +305,8 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		// new(hypervcommon.StepConfigureIp),
 
 		&hypervcommon.StepSetRemoting{
-			Username: DefaultUsername,
-			Password: DefaultPassword,
+			Username: b.config.DefaultUsername,
+			Password: b.config.DefaultPassword,
 		},
 
 		// &hypervcommon.StepCheckRemoting{},
